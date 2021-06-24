19190f1b91e7a69f309ff1fc004d5ea30c927206
jared goldsmith <jgolds@pdx.edu>
2021-06-24 15:11:10 -0700

    The Student toString() now includes the name of the student.

--- student/src/test/java/edu/pdx/cs410J/jgolds/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/jgolds/StudentTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -26,15 +27,26 @@ public class StudentTest
   @Test
   @Disabled
   void daveStudentFromAssignment(){
+    Student dave = getDave();
+
+    assertThat(dave.toString(), equalTo("Dave has a GPA of 3.64 and is taking 3 classes: Algorithms, Operating " +
+            "Systems, and Java. He says \"This class is too much work\"."));
+  }
+
+  private Student getDave() {
     ArrayList<String> classes = new ArrayList<>();
     classes.add("Algorithms");
     classes.add("Operating Systems");
     classes.add("Java");
 
-    Student dave = new Student("Dave", classes, 3.64, "male");
+    return new Student("Dave", classes, 3.64, "male");
+  }
 
-    assertThat(dave.toString(), equalTo("Dave has a GPA of 3.64 and is taking 3 classes: Algorithms, Operating " +
-            "Systems, and Java. He says \"This class is too much work\"."));
+  @Test
+  void toStringContainsStudentName(){
+    Student dave = getDave();
+
+    assertThat(dave.toString(), containsString("Dave"));
   }
 
 }
