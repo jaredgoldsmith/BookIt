8f4eb7781d7407f2f7022a669ee9cde18761d3f5
jared goldsmith <jgolds@pdx.edu>
2021-07-01 13:17:40 -0700

    testing ssh

--- student/src/it/java/edu/pdx/cs410J/jgolds/StudentIT.java
+++ student/src/it/java/edu/pdx/cs410J/jgolds/StudentIT.java
@@ -17,7 +17,7 @@ class StudentIT extends InvokeMainTestCase {
   @Test
   void invokingMainWithNoArgumentsHasExitCodeOf1() {
     InvokeMainTestCase.MainMethodResult result = invokeMain(Student.class);
-    //hey
+    //heyy
     assertThat(result.getExitCode(), equalTo(1));
   }
 
