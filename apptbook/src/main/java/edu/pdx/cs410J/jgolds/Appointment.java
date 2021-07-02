d95eeeed0be4db2519e1c3d485b85e9a7d4917a9
jared goldsmith <jgolds@pdx.edu>
2021-07-02 14:16:58 -0700

    Appointment Book Start

--- apptbook/src/main/java/edu/pdx/cs410J/jgolds/Appointment.java
+++ apptbook/src/main/java/edu/pdx/cs410J/jgolds/Appointment.java
@@ -3,18 +3,35 @@ package edu.pdx.cs410J.jgolds;
 import edu.pdx.cs410J.AbstractAppointment;
 
 public class Appointment extends AbstractAppointment {
+  protected String beginTime;
+  protected String endTime;
+  protected String description;
+
+  public Appointment(){
+    this.beginTime = null;
+    this.endTime = null;
+    this.description = null;
+  }
+  public Appointment(String begin, String end, String description){
+    this.beginTime = begin;
+    this.endTime = end;
+    this.description = description;
+  }
   @Override
   public String getBeginTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return beginTime;
+    //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
   @Override
   public String getEndTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return endTime;
+   // throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
   @Override
   public String getDescription() {
-    return "This method is not implemented yet";
+    //return "This method is not implemented yet";
+    return description;
   }
 }
